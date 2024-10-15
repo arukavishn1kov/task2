cf866c60dd375098bfbe9f64366a20f5a2fca9c5
arukavishn1kov <xeroxanton@gmail.com>
2024-10-15 22:19:14 +0500

    add file

---
+++ module_2_2.py
@@ -0,0 +1,14 @@
+first  = int(input ("Введите число 1: "))
+second = int(input ("Введите число 2: "))
+third = int(input ("Введите число 3: "))
+if first == second and second == third :
+    print(3)
+elif first == second or second == third :
+    print(2)
+else:
+    print(0)
+
+
+
+
+
